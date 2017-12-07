ebc0afa763e8dadb7e114897dd0384d251347e5b
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-07 20:12:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
-def prime(integer)
-  ((2..(Math.sqrt(integer)))).each do |i|
-    return true if integer % i == 0
-  end
-  return false
-end
+def prime(num)
+     for x in 2..num-1
+        print x
+        if num % x == 0
+           return false
+        else
+           return true
+        end
+     end
+  end 
